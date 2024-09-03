6e6f97630d2510da4f7bc61c90ba02e128744125
Cleiton Rieke <cleitonrieke@gmail.com>
2024-09-02 21:14:09 -0300

    ajustes para resolver problema de cors

--- db/db.go
+++ db/db.go
@@ -14,14 +14,11 @@ func Init() *gorm.DB {
 	dbPass := os.Getenv("DB_PASS")
 	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
-	//dbURL := "postgres://" + dbUser + ":" + dbPass + "@" + dbHost + ":" + dbPort + "/" + dbName + "?sslmode=disable"
 	dbURL := "postgres://" + dbUser + ":" + dbPass + "@" + dbHost + ":" + dbPort + "/" + dbName
-	println(dbURL)
 
 	db, err := gorm.Open(postgres.Open(dbURL), &gorm.Config{})
 
 	if err != nil {
-		println("URL:" + dbURL)
 		println("Error connecting to database: ", err)
 		log.Fatalln(err)
 	}
